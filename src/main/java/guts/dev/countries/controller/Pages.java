b6aa0d3b99126c64ed13487652477026cff53360
gitguts <jonathanbirkstrom@gmail.com>
2025-10-03 11:12:11 +0200

    Added a beautiful welcome page

--- src/main/java/guts/dev/countries/controller/Pages.java
+++ src/main/java/guts/dev/countries/controller/Pages.java
@@ -1,14 +1,15 @@
 package guts.dev.countries.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Pages {
 
     @GetMapping("/")
-    public String welcome(Model model) {
-        return "Welcome to the countries website!";
+    public String index(Model model) {
+        return "home/index";
     }
 }
